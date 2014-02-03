f90d79aed4bc77bc34b6b71a310a766f68913717
Nitya Dhanushkodi <ndhanushkodi@ndhanushkodi-Latitude-E6430.(none)>
2014-02-03 04:18:01 -0500

    grid done

--- hw2/grid.py
+++ hw2/grid.py
@@ -2,7 +2,22 @@
 """
 Created on Mon Feb  3 03:25:33 2014
 
-@author: ndhanushkodi
+@author: Nitya Dhanushkodi
+SoftwareDesign Homework 2
 """
 
-grid test
+row1 = ('+' + 4*'-')*2 + '+'
+rowmid = '|    |    |'
+print row1
+
+
+def print_four():
+    print rowmid
+    print rowmid
+    print rowmid
+    print rowmid
+    
+print_four()
+print row1
+print_four()
+print row1
